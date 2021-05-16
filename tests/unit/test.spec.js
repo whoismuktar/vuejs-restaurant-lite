3f45b34cecf948e6fe227be54e722e2a3a96640a
mayunited12 <mayunited12@yahoo.com>
2021-05-16 22:17:41 +0100

    e2e testing

--- tests/unit/test.spec.js
+++ tests/unit/test.spec.js
@@ -1,13 +1,31 @@
 import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import themeMode from '@/components/theme/themeMode';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).to.include(msg)
+describe('themeMode.vue', () => {
+  it('isDarkMode should be false', () => {
+    const wrapper = shallowMount(themeMode)
+
+    expect(wrapper.find('.toggle_label').text()).to.contain('Light');
+    expect(wrapper.vm.isDarkMode).to.be.false;
+  }),
+  it('set Dark Mode to true', () => {
+    const wrapper = shallowMount(themeMode)
+    wrapper.find('#switchInput').setChecked();
+
+    expect(wrapper.vm.isDarkMode).to.be.true;
+  })
+  it('localStorage is true on check', () => {
+    const wrapper = shallowMount(themeMode)
+    wrapper.find('#switchInput').setChecked();
+    
+    expect(JSON.parse(window.localStorage.getItem("isDarkMode"))).to.be.true;
+  })
+  it('localStorage is false on uncheck', () => {
+    const wrapper = shallowMount(themeMode)
+    wrapper.find('#switchInput').setChecked();
+    wrapper.find('#switchInput').setChecked(false);
+
+    expect(JSON.parse(window.localStorage.getItem("isDarkMode"))).to.be.false;
   })
 })
