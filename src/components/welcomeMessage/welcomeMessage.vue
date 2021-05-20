6a421409cc8d8d932030a53854de3c9f206319b1
mayunited12 <mayunited12@yahoo.com>
2021-05-20 23:05:00 +0100

    convert welcome messgae to component

---
+++ src/components/welcomeMessage/welcomeMessage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="welcomeMessage">
+      <p>{{ welcomeMessage }}</p>
+  </div>
+</template>
+
+<script>
+import dayjs from "dayjs";
+var advancedFormat = require('dayjs/plugin/advancedFormat')
+dayjs.extend(advancedFormat)
+
+export default {
+  name: 'WelcomeMessage',
+  data () {
+    return {
+      currentTime2: dayjs().format('dddd, MMMM Do YYYY, h:mm:ss a')
+    }
+  },
+  computed: {
+    welcomeMessage () {
+      return 'Welcome! Your local time iss: ' + this.currentTime2;
+    }
+  },
+  mounted() {
+    const setTimer = () => setTimeout(() => {
+      this.currentTime = dayjs().format('dddd, MMMM Do YYYY, h:mm:ss a');
+      setTimer();
+    }, 1000);
+    setTimer();
+  }
+}
+</script>
+
+<style>
+
+</style>
