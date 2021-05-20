b534abe3649a398a44ca7eb3d92e0d1ccc8f4e27
mayunited12 <mayunited12@yahoo.com>
2021-05-20 23:03:10 +0100

    update view to re-useable view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,16 @@
 <template>
   <div class="page page__home">
-    <p>{{ welcomeMessage }}</p>
+    <welcome-message></welcome-message>
   </div>
 </template>
 
 <script>
-import moment from 'moment';
+import welcomeMessage from "@/components/welcomeMessage/welcomeMessage";
 
 export default {
   name: 'Home',
-  data () {
-    return {
-      currentTime: moment().format('dddd, MMMM Do YYYY, h:mm:ss a')
-    }
-  },
-  computed: {
-    welcomeMessage () {
-      return 'Welcome! Your local time is: ' + this.currentTime;
-    }
-  },
-  mounted() {
-    const setTimer = () => setTimeout(() => {
-      this.currentTime = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
-      setTimer();
-    }, 1000);
-    setTimer();
+  components: {
+    welcomeMessage
   }
 }
 </script>
